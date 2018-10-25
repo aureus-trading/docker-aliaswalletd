464b7f6a70094421531b83d3dfaba2cceead5487
HLXEasy <hlxeasy@gmail.com>
2018-10-25 17:01:17 +0200

    Fixed variable name and syntax warnings

--- bootstrap-host.sh
+++ bootstrap-host.sh
@@ -4,7 +4,7 @@
 #
 set -ex
 
-BTC_IMAGE=${BTC_IMAGE:-spectreproject/spectrecoind}
+SPECTRECOIN_IMAGE=${SPECTRECOIN_IMAGE:-spectreproject/spectrecoind}
 
 distro=$1
 shift
@@ -14,7 +14,7 @@ memtotal=$(grep ^MemTotal /proc/meminfo | awk '{print int($2/1024) }')
 #
 # Only do swap hack if needed
 #
-if [ $memtotal -lt 2048 -a $(swapon -s | wc -l) -lt 2 ]; then
+if [ ${memtotal} -lt 2048 -a $(swapon -s | wc -l) -lt 2 ]; then
     fallocate -l 2048M /swap || dd if=/dev/zero of=/swap bs=1M count=2048
     mkswap /swap
     grep -q "^/swap" /etc/fstab || echo "/swap swap swap defaults 0 0" >> /etc/fstab
@@ -40,13 +40,13 @@ docker rm spectrecoind-node 2>/dev/null || true
 stop docker-spectrecoind 2>/dev/null || true
 
 # Always pull remote images to avoid caching issues
-if [ -z "${BTC_IMAGE##*/*}" ]; then
-    docker pull $BTC_IMAGE
+if [ -z "${SPECTRECOIN_IMAGE##*/*}" ]; then
+    docker pull ${SPECTRECOIN_IMAGE}
 fi
 
 # Initialize the data container
 docker volume create --name=spectrecoind-data
-docker run -v spectrecoind-data:/spectrecoin --rm $BTC_IMAGE spectrecoin_init
+docker run -v spectrecoind-data:/spectrecoin --rm ${SPECTRECOIN_IMAGE} spectrecoin_init
 
 # Start spectrecoind via upstart and docker
 curl https://raw.githubusercontent.com/spectreproject/docker-spectrecoind/master/upstart.init > /etc/init/docker-spectrecoind.conf
@@ -54,4 +54,4 @@ start docker-spectrecoind
 
 set +ex
 echo "Resulting spectrecoin.conf:"
-docker run -v spectrecoind-data:/spectrecoin --rm $BTC_IMAGE cat /spectrecoin/.spectrecoin/spectrecoin.conf
+docker run -v spectrecoind-data:/spectrecoin --rm ${SPECTRECOIN_IMAGE} cat /spectrecoin/.spectrecoin/spectrecoin.conf
